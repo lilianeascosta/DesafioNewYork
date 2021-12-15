82a7815c0db302271baff123371d1e032affb4a8
lilianeascosta <lilianecosta767@hotmail.com>
2021-12-14 22:18:01 -0300

    fazendo deploy da aplicacao

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/lilianeascosta/DesafioNewYork.git', // Update to point to your repository  
+        user: {
+            name: 'lilianeascosta', // update to use your name
+            email: 'lilianecosta767@hotmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
