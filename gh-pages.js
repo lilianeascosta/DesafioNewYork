8159d1056345802ef12474245a4a9a70894ea3ad
lilianeascosta <lilianecosta767@hotmail.com>
2021-12-14 22:29:31 -0300

    commit description

--- gh-pages.js
+++ gh-pages.js
@@ -8,7 +8,8 @@ ghpages.publish(
         user: {
             name: 'lilianeascosta', // update to use your name
             email: 'lilianecosta767@hotmail.com' // Update to use your email
-        }
+        },
+		dotfiles: true
     },
     () => {
         console.log('Deploy Complete!')
